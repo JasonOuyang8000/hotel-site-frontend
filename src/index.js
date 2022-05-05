69a3fb8b14a5551cc33ac3374262e1d955842c2c
Christine Yoo <christineyoo@Christines-MBP.attlocal.net>
2022-05-05 00:44:12 -0700

    chakra setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { colors } from "./themes/themes";
+
+const theme = extendTheme({ colors });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ChakraProvider>
 );
