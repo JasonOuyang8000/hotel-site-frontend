b5941290a39e3bb19f7a7aef29e59123ea3390ef
Christine Yoo <christineyoo@Christines-MBP.attlocal.net>
2022-05-05 23:38:01 -0700

    created a landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  Box,
+  Image,
+  Button,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <Box>
+      <Flex
+        align="center"
+        justify={{ base: "center", md: "space-around", xl: "space-between" }}
+        direction={{ base: "column-reverse", md: "row" }}
+        wrap="no-wrap"
+        minH="70vh"
+        px={8}
+        mb={16}
+      >
+        <Stack></Stack>
+      </Flex>
+    </Box>
+  );
 };
 
 export default LandingPage;
