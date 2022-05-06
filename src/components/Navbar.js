e4dc8dcda1c6a9ab5cbf7927ee9059e7c0a2f573
Jason  Ouyang <jasonouyang@Jasons-MBP.attlocal.net>
2022-05-05 23:57:11 -0700

    fix navbar alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,43 +3,40 @@ import { Box, Flex, Button, Heading, HStack, Link } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <Box>
+    <Box
+      px="200px"
+      py="20px"
+      borderBottom={1}
+      borderStyle={"solid"}
+      borderColor={"font.800"}
+      width="full"
+      alignItems="flex-end"
+    >
       <Flex
-        px="200px"
-        py="20px"
-        borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={"font.800"}
-        width="full"
-        alignItems="flex-end"
+        color="font.800"
+        minWidth="max-content"
+        alignItems="center"
         justifyContent="space-between"
       >
-        <Flex
-          color="font.800"
-          minWidth="max-content"
-          alignItems="center"
-          gap="2"
-        >
-          <Heading mr="60px" fontSize={20} letterSpacing="1.5px">
-            Hotel Search
-          </Heading>
-          <HStack justify={"flex-end"} spacing="40px">
-            <Link>Home</Link>
-            <Link>About Us</Link>
-            <Link>Sign Up</Link>
-            <Button
-              gap="2"
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"sm"}
-              fontWeight={600}
-              color={"white"}
-              bg={"font.700"}
-              variant="solid"
-            >
-              Login
-            </Button>
-          </HStack>
-        </Flex>
+        <Heading mr="auto" fontSize={20} letterSpacing="1.5px">
+          Hotel Search
+        </Heading>
+        <HStack justify={"flex-end"} spacing="40px">
+          <Link>Home</Link>
+          <Link>About Us</Link>
+          <Link>Sign Up</Link>
+          <Button
+            gap="2"
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bg={"font.700"}
+            variant="solid"
+          >
+            Login
+          </Button>
+        </HStack>
       </Flex>
     </Box>
   );
